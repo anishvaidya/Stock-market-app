fa0509deeda2d069aefa40c9676f78505e887f6e
“anishvaidya” <aavaidya@usc.edu>
2020-11-03 09:54:33 -0800

    fixed change in portfolio

--- angular-frontend/src/app/components/cards/myportfolio/myportfolio.component.ts
+++ angular-frontend/src/app/components/cards/myportfolio/myportfolio.component.ts
@@ -36,7 +36,7 @@ export class MyportfolioComponent implements OnChanges {
       if (this.currentPriceOfStocks[i].ticker == this.portfolio.value.ticker){
         console.log("true happened");
         this.currentPrice = {"last": this.currentPriceOfStocks[i]["last"]};
-        this.change = ((this.portfolio.value.totalCost / this.portfolio.value.quantity) - this.currentPriceOfStocks[i].last).toFixed(2);
+        this.change = (-(this.portfolio.value.totalCost / this.portfolio.value.quantity) + this.currentPriceOfStocks[i].last).toFixed(2);
       }
     }
     console.log(this.currentPrice);
